95999531ac7afabbde1ae7b37538694a8cd4085e
Andrew Wason <rectalogic@rectalogic.com>
2020-04-25 17:42:52 -0400

    Don't render local audio track

--- src/components/AudioTrack/AudioTrack.tsx
+++ src/components/AudioTrack/AudioTrack.tsx
@@ -1,31 +1,15 @@
 import React, { useEffect, useMemo, useRef } from 'react';
 import { useThree } from 'react-three-fiber';
 import * as THREE from 'three';
-import { LocalAudioTrack as ILocalAudioTrack, RemoteAudioTrack as IRemoteAudioTrack, Track } from 'twilio-video';
+import { RemoteAudioTrack as IRemoteAudioTrack, Track } from 'twilio-video';
 import { AUDIO_REF_DISTANCE } from '../../Globals';
-
-interface LocalAudioTrackProps {
-  track: ILocalAudioTrack;
-}
-
-export function LocalAudioTrack({ track }: LocalAudioTrackProps) {
-  const audio = useMemo(() => document.createElement('audio'), []);
-
-  useEffect(() => {
-    track.attach(audio);
-    return () => {
-      track.detach(audio);
-    };
-  }, [track, audio]);
-  return null;
-}
 
 interface RemoteAudioTrack3DProps {
   track: IRemoteAudioTrack;
   priority: Track.Priority;
 }
 
-export function RemoteAudioTrack3D({ track, priority }: RemoteAudioTrack3DProps) {
+export default function RemoteAudioTrack3D({ track, priority }: RemoteAudioTrack3DProps) {
   const ref = useRef<THREE.PositionalAudio>(null);
   const { camera } = useThree();
   const audio = useMemo(() => document.createElement('audio'), []);
