7fb5a368434deb54203e21029f2973bfba1511e3
Andrew Wason <rectalogic@rectalogic.com>
2020-04-26 17:12:22 -0400

    Move info divs into main DOM and track a map of refs for Dom3D

--- src/components/Dom3D/Dom3D.tsx
+++ src/components/Dom3D/Dom3D.tsx
@@ -1,24 +1,22 @@
-import React, { useState, useEffect, useRef } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useRef } from 'react';
 import * as THREE from 'three';
 import { useThree, useFrame, ReactThreeFiber } from 'react-three-fiber';
 
 interface Dom3DProps {
+  element: HTMLElement | null;
   position: ReactThreeFiber.Vector3;
   scale: number;
-  children: React.ReactNode;
 }
 
-export default function Dom3D({ position, scale, children }: Dom3DProps) {
-  const { gl, scene, camera, size } = useThree();
+export default function Dom3D({ element, position, scale }: Dom3DProps) {
+  const { camera, size } = useThree();
   const group = useRef<THREE.Object3D>();
-  const [root] = useState(() => document.createElement('div'));
   const vector = new THREE.Vector3();
   const vector4 = new THREE.Vector4();
   const oldVector = useRef(new THREE.Vector3(1, 1, 0));
   const epsilon = 0.001;
 
-  function updatePosition(group: THREE.Object3D, epsilon: number) {
+  function updatePosition(group: THREE.Object3D, element: HTMLElement, epsilon: number) {
     vector.setFromMatrixPosition(group.matrixWorld);
 
     // See Vector3.project() but we need to keep Vector4.w
@@ -44,38 +42,18 @@ export default function Dom3D({ position, scale, children }: Dom3DProps) {
       vector.y = THREE.MathUtils.clamp(vector.y, 0, size.height);
       // Dim if offscreen
       if (vector.x <= 0 || vector.x >= size.width || vector.y <= 0 || vector.y >= size.height) {
-        root.style.opacity = '0.3';
+        element.style.opacity = '0.3';
       } else {
-        root.style.opacity = '1';
+        element.style.opacity = '1';
       }
-      root.style.transform = 'translate3d(' + vector.x + 'px,' + vector.y + 'px,0) scale(' + scale + ')';
+      element.style.transform = 'translate3d(' + vector.x + 'px,' + vector.y + 'px,0) scale(' + scale + ')';
     }
     oldVector.current.copy(vector);
   }
 
-  useEffect(() => {
-    // Offset so center point is bottom middle
-    ReactDOM.render(<div style={{ transform: 'translate3d(-50%,-100%,0)' }}>{children}</div>, root);
-    if (gl.domElement.parentNode) {
-      gl.domElement.parentNode.appendChild(root);
-    }
-
-    if (group.current) {
-      scene.updateMatrixWorld();
-      root.style.cssText = 'position:absolute;top:0;left:0;';
-    }
-
-    return () => {
-      if (gl.domElement.parentNode) {
-        gl.domElement.parentNode.removeChild(root);
-      }
-      ReactDOM.unmountComponentAtNode(root);
-    };
-  });
-
   useFrame(() => {
-    if (group.current) {
-      updatePosition(group.current, epsilon);
+    if (group.current && element) {
+      updatePosition(group.current, element, epsilon);
     }
   });
 
