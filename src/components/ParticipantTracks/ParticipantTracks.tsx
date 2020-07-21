906a24223662ccd55d5703ebc238832112c4aca4
Andrew Wason <rectalogic@rectalogic.com>
2020-07-21 09:45:29 -0400

    Move placeholder code

--- src/components/ParticipantTracks/ParticipantTracks.tsx
+++ src/components/ParticipantTracks/ParticipantTracks.tsx
@@ -30,6 +30,7 @@ interface LocalParticipantVideoTracksProps {
   participant: LocalParticipant;
 }
 export function LocalParticipantVideoTracks({ participant }: LocalParticipantVideoTracksProps) {
+  // return placeholder(participant);
   const publications = usePublications(participant);
   return (
     <>
@@ -46,6 +47,7 @@ interface RemoteParticipantVideoTracksProps {
   participant: RemoteParticipant;
 }
 export function RemoteParticipantVideoTracks({ participant }: RemoteParticipantVideoTracksProps) {
+  // return placeholder(participant);
   const publications = usePublications(participant);
   return (
     <>
@@ -68,6 +70,7 @@ export function RemoteParticipantAudioTracks({
   participant,
   participant3D,
 }: RemoteParticipantAudioTracksProps) {
+  // return <></>;
   const publications = usePublications(participant);
   return (
     <>
@@ -111,3 +114,34 @@ export function RemoteParticipantDataTracks({
     </>
   );
 }
+
+// Uncomment the following for screenshots
+
+// // eslint-disable-next-line
+// import { Participant } from 'twilio-video';
+// // eslint-disable-next-line
+// import { styled } from '@material-ui/core/styles';
+
+// const Placeholder = styled('img')({
+//   width: '100%',
+//   maxHeight: '100%',
+//   objectFit: 'contain',
+//   background: '#C2C2C2',
+// });
+
+// const name2Face: { [key: string]: string } = {
+//   "Karen": "https://images.generated.photos/Ql60mpl0vLUSqBZ1YN7kiKROV8ZSqV5dd9pbDU1z0w4/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA4ODAwNTQu/cG5n.png",
+//   "Bill": "https://images.generated.photos/QEEt96v4Ysf_AtazThxMj2A1vxaTcs_TdA5-I6w2aqo/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzAxMDYyNDMu/cG5n.png",
+//   "Cindy": "https://images.generated.photos/-rUHHw-aHz_infxRD6JrPgl13WRcyrbu4JaOxPW3KVI/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA4OTAyMjMu/cG5n.png",
+//   "Roger": "https://images.generated.photos/IxBBO2x9K-168_taejHqrxT9y2sfFcESFCrcchLi1LQ/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA5MjY5MTAu/cG5n.png",
+//   "Steve": "https://images.generated.photos/UkDKPouDDbZiOLHcPk6vqOSNsdgFRbpoAYmrdoSXucI/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzAzMDk1ODUu/cG5n.png",
+//   "Stephanie": "https://images.generated.photos/sgByAx3zZf0qh54SKtPZn9i_xmTW_FKa39INZhVDznQ/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA5Nzk0OTku/cG5n.png",
+//   "Laura": "https://images.generated.photos/y-uCbEShljKbhjxrrnktySdcaQ5H_w9eTZd6dnYpIis/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzAzNTk2NTUu/cG5n.png",
+//   "Lianna": "https://images.generated.photos/SzA-AiznHIz0pnkh7vPafp_QMVb0A-5oS80A9XL1aJo/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA0MjYyNzQu/cG5n.png",
+//   "Chad": "https://images.generated.photos/qKfnNXm0zdZ55geqDWA4pumL0dmUYkysSoDesvbzIwg/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA5NTk2MjMu/cG5n.png",
+//   "Jane": "https://images.generated.photos/0wwxAyBrw9rA04-qJ3Q-RBwjTMGLfWa1QsQh0mIaqsM/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA0Nzc4MDEu/cG5n.png",
+// };
+
+// function placeholder(participant: Participant) {
+//   return <Placeholder src={name2Face[participant.identity]} />
+// }
--- src/components/VideoTrack/VideoTrack.tsx
+++ src/components/VideoTrack/VideoTrack.tsx
@@ -42,37 +42,3 @@ export default function VideoTrack({ track, isLocal, priority }: VideoTrackProps
 
   return <Video ref={ref} style={style} />;
 }
-
-// For screenshots, comment out the above and use this
-
-// const faces = [
-//   "https://images.generated.photos/Ql60mpl0vLUSqBZ1YN7kiKROV8ZSqV5dd9pbDU1z0w4/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA4ODAwNTQu/cG5n.png",
-//   "https://images.generated.photos/QEEt96v4Ysf_AtazThxMj2A1vxaTcs_TdA5-I6w2aqo/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzAxMDYyNDMu/cG5n.png",
-//   "https://images.generated.photos/-rUHHw-aHz_infxRD6JrPgl13WRcyrbu4JaOxPW3KVI/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA4OTAyMjMu/cG5n.png",
-//   "https://images.generated.photos/IxBBO2x9K-168_taejHqrxT9y2sfFcESFCrcchLi1LQ/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA5MjY5MTAu/cG5n.png",
-//   "https://images.generated.photos/UkDKPouDDbZiOLHcPk6vqOSNsdgFRbpoAYmrdoSXucI/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzAzMDk1ODUu/cG5n.png",
-//   "https://images.generated.photos/sgByAx3zZf0qh54SKtPZn9i_xmTW_FKa39INZhVDznQ/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA5Nzk0OTku/cG5n.png",
-//   "https://images.generated.photos/y-uCbEShljKbhjxrrnktySdcaQ5H_w9eTZd6dnYpIis/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzAzNTk2NTUu/cG5n.png",
-//   "https://images.generated.photos/SzA-AiznHIz0pnkh7vPafp_QMVb0A-5oS80A9XL1aJo/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA0MjYyNzQu/cG5n.png",
-//   "https://images.generated.photos/qKfnNXm0zdZ55geqDWA4pumL0dmUYkysSoDesvbzIwg/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA5NTk2MjMu/cG5n.png",
-//   "https://images.generated.photos/0wwxAyBrw9rA04-qJ3Q-RBwjTMGLfWa1QsQh0mIaqsM/rs:fit:512:512/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Ry/YW5zcGFyZW50X3Yz/L3YzXzA0Nzc4MDEu/cG5n.png",
-// ];
-// const trackFaces = new Map<IVideoTrack, string>();
-
-// const Placeholder = styled('img')({
-//   width: '100%',
-//   maxHeight: '100%',
-//   objectFit: 'contain',
-//   background: '#C2C2C2',
-// });
-
-// export default function VideoTrack({ track, isLocal, priority }: VideoTrackProps) {
-//   let url = trackFaces.get(track);
-//   if (!url) {
-//     url = faces.shift();
-//     if (url) {
-//       trackFaces.set(track, url);
-//     }
-//   }
-//   return url ? <Placeholder src={url} /> : null;
-// }
